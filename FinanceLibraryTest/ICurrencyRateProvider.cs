5beb2c67b9221eddb6025be7ddabfea1eae0710a
Илья Кабанов <tolatolkin@gmail.com>
2025-10-31 16:29:04 +0500

    добавлен интерфейс

---
+++ FinanceLibraryTest/ICurrencyRateProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceLibraryTest
+{
+    internal interface ICurrencyRateProvider
+    {
+        IReadOnlyDictionary<string, double> GetCurrentRates();
+        IEnumerable<(DateTime Date, string Currency, double Rate)> GetRatesHistory();
+    }
+}
